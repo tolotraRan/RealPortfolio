b770b3bde1456748d10d14aa58282f00524a86f5
tolotraRan <ranarisontolotra@gmail.com>
2024-02-13 21:41:34 +0300

    nab commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,11 +1,23 @@
 import { FC } from "react";
-import { allDataType } from "../shared/types";
+import skillsData from "../shared/skillsData.json";
+import Image from 'next/image';
+
+interface Skill {
+  title: string;
+  image: string;
+}
 
 interface SkillsProps {
-  skills: allDataType["skills"];
+  skills: Skill[];
 }
 
 const Skills: FC<SkillsProps> = ({ skills }) => {
+  // Séparer les compétences en fonction de leur catégorie
+  const languages = skills.filter(skill => ["HTML", "CSS", "JavaScript", "PHP", "C#"].includes(skill.title));
+  const frameworks = skills.filter(skill => ["REACTJS", "Tailwind","Bootstrap", "NODE", "Express","LARAVEL"].includes(skill.title));
+  const databases = skills.filter(skill => ["MYSQL", "POSTEGRESQL", "MONGODB"].includes(skill.title));
+  const others = skills.filter(skill => !["XD", "Git"].includes(skill.title));
+
   return (
     <div id="Skills" className="h-screen border-cyan-100 mb-56">
       <div className="lg:min-h-[70vh] px-[5vw] py-[80px] flex flex-col items-center justify-center z-[4]">
@@ -25,27 +37,96 @@ const Skills: FC<SkillsProps> = ({ skills }) => {
         >
           Technologies and languages that I use to make my product everyday
         </p>
-        <div className="grid justify-center grid-cols-4 md:grid-cols-6 gap-y-[10px] md:gap-y-[20px]">
-          {skills.map((skill) => (
-            <a
-              key={skill.link}
-              href={skill.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="item flex flex-col items-center justify-around group"
-            >
-              <img
-                className="w-[60px] h-[60px] transition duration-300 group-hover:scale-110"
-                src={skill.image}
-                alt=""
-              />
-              <p className="text-center">{skill.title}</p>
-            </a>
-          ))}
+        <div className="grid grid-cols-4 gap-10 ml-32">
+          <div className="col-span-1">
+            <div className="grid grid-cols-2 gap-y-4">
+              <h2 className="text-xl font-bold mb-4 col-span-2">Languages</h2>
+              {languages.map((skill, index) => (
+                <div
+                  key={`language-${index}`}
+                  className="item flex flex-col items-center justify-around group"
+                >
+                  <Image
+                    className="w-[60px] h-[60px] transition duration-300 group-hover:scale-110"
+                    src={`/Images/${skill.image}`} 
+                    alt={skill.title}
+                    width={60} 
+                    height={60} 
+                  />
+                  <p className="text-center">{skill.title}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="col-span-1">
+            <div className="grid grid-cols-2 gap-y-4">
+              <h2 className="text-xl font-bold mb-4 col-span-2">Frameworks</h2>
+              {frameworks.map((skill, index) => (
+                <div
+                  key={`framework-${index}`}
+                  className="item flex flex-col items-center justify-around group"
+                >
+                  <Image
+                    className="w-[60px] h-[60px] transition duration-300 group-hover:scale-110"
+                    src={`/Images/${skill.image}`} 
+                    alt={skill.title}
+                    width={60} 
+                    height={60} 
+                  />
+                  <p className="text-center">{skill.title}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="col-span-1">
+            <div className="grid grid-cols-2 gap-y-4">
+              <h2 className="text-xl font-bold mb-4 col-span-2">Databases</h2>
+              {databases.map((skill, index) => (
+                <div
+                  key={`database-${index}`}
+                  className="item flex flex-col items-center justify-around group"
+                >
+                  <Image
+                    className="w-[60px] h-[60px] transition duration-300 group-hover:scale-110"
+                    src={`/Images/${skill.image}`} 
+                    alt={skill.title}
+                    width={60} 
+                    height={60} 
+                  />
+                  <p className="text-center">{skill.title}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="col-span-1">
+            <div className="grid grid-cols-2 gap-y-4">
+              <h2 className="text-xl font-bold mb-4 col-span-2">Others</h2>
+              {others.map((skill, index) => (
+                <div
+                  key={`other-${index}`}
+                  className="item flex flex-col items-center justify-around group"
+                >
+                  <Image
+                    className="w-[60px] h-[60px] transition duration-300 group-hover:scale-110"
+                    src={`/Images/${skill.image}`} 
+                    alt={skill.title}
+                    width={60} 
+                    height={60} 
+                  />
+                  <p className="text-center">{skill.title}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Skills;
+// Pass the skills data from the imported JSON file to the Skills component
+const SkillsContainer: FC = () => {
+  return <Skills skills={skillsData} />;
+};
+
+export default SkillsContainer;
